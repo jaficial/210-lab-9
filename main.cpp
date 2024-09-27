c6d978c5633d5d72febe0006213bf5200e694b2b
Jed Aficial's PC <jedaficial1@gmail.com>
2024-09-26 17:56:28 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for ()
+    for (int i = 0; i <= 100; i++)
         cout << i << " ";
 
     return 0;
